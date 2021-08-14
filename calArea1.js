90f363b572402782c02e7516d21477b9ab594003
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-14 16:29:17 +0530

    input output processing done

--- calArea1.js
+++ calArea1.js
@@ -1,1 +1,28 @@
-alert("working")
+var userInput= document.querySelectorAll(".userInput")
+var calAreaBtn = document.querySelector("#calAreaBtn")
+var outputDiv= document.querySelector(".output-div")
+var errorMsg = document.querySelector(".errorDiv")
+
+function calAreaBtnHandler(){
+  var base= Number(userInput[0].value)
+  var height = Number(userInput[1].value)
+  console.log(base);
+  console.log(height);
+
+  if(base==""|| height==""){
+    errorDisplay("please fill the inputs")
+  }else {
+    calculateAreaBase(base,height)
+  }
+}
+
+function calculateAreaBase(b,h) {
+  outputDiv.innerText= (b+h)/2
+}
+
+function errorDisplay(msg) {
+  errorMsg.style.display="block"
+  errorMsg.innerText=msg
+}
+
+calAreaBtn.addEventListener("click",calAreaBtnHandler)
