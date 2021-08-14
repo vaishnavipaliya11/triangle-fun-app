1245a4f414c11ea63996bdb2540d32ab3a431c09
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-14 12:37:33 +0530

    test: input and outputs

--- calAngle.js
+++ calAngle.js
@@ -1,1 +1,35 @@
-alert("working")
+var userInputs = document.querySelectorAll(".angle-input")
+var btnCheck = document.querySelector("#btn-check")
+var outputDiv= document.querySelector(".output-div")
+var errorDiv = document.querySelector(".errorDiv")
+
+function checkBtnHandler() {
+  var angleOne = Number(userInputs[0].value)
+  var angleTwo = Number(userInputs[1].value)
+  var angleThree = Number(userInputs[2].value)
+
+  if(angleOne==""|| angleTwo==""||angleThree==""){
+    errorDisplay("fill all the angles")
+  }else {
+    triangleFormed(angleOne,angleTwo,angleThree)
+    errorDiv.style.display="none"
+  }
+}
+
+
+function triangleFormed(angleOne,angleTwo,angleThree){
+var sum= angleOne+angleTwo+angleThree
+
+  if(sum ===180){
+    outputDiv.innerText="it makes a triangle"
+  }else {
+    outputDiv.innerText="don't make a triangle"
+  }
+}
+
+function errorDisplay(msg) {
+  errorDiv.style.display="block"
+  errorDiv.innerText=msg
+}
+
+btnCheck.addEventListener('click',checkBtnHandler)
